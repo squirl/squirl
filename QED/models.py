2bf795390b532172115d1b5efd157d31754e7dd3
AbdrodeDomeris <aklapper4@gmail.com>
2015-03-29 13:00:48 -0500

    A few additions to the database. Will implement model mixins later. Just focused on making a rough layout for now

--- QED/models.py
+++ QED/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#will implement model mixins shortly
 
 class Location (models.Model):
     name = models.CharField(max_length = 100)
