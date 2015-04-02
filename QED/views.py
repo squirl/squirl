ecc91fa8cef9b8b8daed4d93d06ae0580be8727b
AbdrodeDomeris <aklapper4@gmail.com>
2015-04-01 21:02:51 -0500

    Added a few more folders and started implementing the homepage

--- QED/views.py
+++ QED/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Sorry, Andrew has not finished this page yet.")
 # Create your views here.
--- squirl/urls.py
+++ squirl/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
+                       
     # url(r'^$', 'squirl.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^QED/', include('QED.urls')),
 )
