fdbb387014b05c5f24666a8e56c038fdd85222f5
AbdrodeDomeris <aklapper4@gmail.com>
2015-08-19 21:01:20 -0500

    removed 1 dajaxice import

--- squirldev/urls.py
+++ squirldev/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 dajaxice_autodiscover()
 
