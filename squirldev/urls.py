a9920cc3c24608845b3ac89a4f58ce215c0cb2e2
AbdrodeDomeris <aklapper4@gmail.com>
2015-08-19 21:03:10 -0500

    Removed another dajaxice import

--- squirldev/urls.py
+++ squirldev/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-dajaxice_autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
